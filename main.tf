92272b5cea9461b4e885bba6c983621b98557330
Gauraj <gaurajjadhav0@gmail.com>
2025-05-30 17:54:39 +0530

    Added DevOps Task : Docker container with Terraform

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine" # For Windows
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx-server"
+  image = docker_image.nginx.name
+
+    ports {
+    internal = 80
+    external = 8888
+  }
+
+}
